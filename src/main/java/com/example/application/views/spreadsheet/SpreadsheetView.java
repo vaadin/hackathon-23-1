8c711184448ec0db359e611be1f2a08d56dc7bec
start.vaadin.com <no-reply@vaadin.com>
2022-06-06 11:22:01 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/spreadsheet/SpreadsheetView.java
@@ -0,0 +1,32 @@
+package com.example.application.views.spreadsheet;
+
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.RouteAlias;
+
+@PageTitle("Spreadsheet")
+@Route(value = "hello")
+@RouteAlias(value = "")
+public class SpreadsheetView extends VerticalLayout {
+
+    public SpreadsheetView() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        add(new H2("This place intentionally left empty"));
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
